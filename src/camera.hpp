bd1050c33311c1c992840b71f13af84252bc3e03
Nick VanderPyle <NickVanderPyle@users.noreply.github.com>
2024-08-18 19:20:23 -0500

    Slow touch movement again.

--- src/camera.hpp
+++ src/camera.hpp
@@ -22,7 +22,7 @@ class Camera {
     float yaw = -90.0f;  // Yaw is initialized to -90 degrees since a forward vector points in the negative Z direction by default.
     float pitch = 0.0f;
 
-    float sensitivity = 0.4f;
+    float sensitivity = 0.3f;
 
     glm::mat4x4 projectionMatrix = glm::mat4x4(1);
     glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
